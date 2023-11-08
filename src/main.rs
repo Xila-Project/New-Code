040fb25b7bc68a252b312b2cd708142a8ef45e00
Alix ANNERAUD <alix@anneraud.fr>
2023-11-09 00:49:21 +0100

    Play with wamr virtual machine and FFI

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,20 @@ use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, alway
 mod File_system;
 use File_system::*;
 
+mod Log;
+
+use std::{
+    ffi::{CStr, CString},
+    os::raw::c_char,
+};
+
+use wamr_sys::*;
+
+#[no_mangle]
+pub extern "C" fn carresser_le_capitaine() -> i32 {
+    println!("Carresser le capitaine !!!");
+    18
+}
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -19,13 +33,121 @@ fn main() {
     #[cfg(target_os = "espidf")]
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    #[cfg(target_os = "espidf")]
-    log::info!("Hello, world!");
+    let mut wasm_module_bytes = include_bytes!("fuck.wasm");
 
-    #[cfg(target_os = "linux")]
-    println!("Hello, world!");
-  
+    let wasm_module_bytes = include_bytes! {"fuck.wasm"};
+    let mut wasm_module_bytes_2: [u8; 100_000] = [0; 100_000];
+    wasm_module_bytes_2[..wasm_module_bytes.len()].clone_from_slice(wasm_module_bytes);
 
+    let mut error_buf: Vec<i8> = vec![32; 128];
+    const STACK_SIZE: u32 = 8092;
+
+    unsafe {
+        if !wasm_runtime_init() {
+            Error!("Failed to init WASM runtime");
+        }
+
+
+        let symbol = CString::new("carresser_le_capitaine").unwrap();
+        let signature = CString::new("()i").unwrap(); 
+        
+
+        let mut ntv: [NativeSymbol; 1] = [{ 
+            {
+                NativeSymbol {
+                    symbol: symbol.as_ptr() as *const c_char,
+                    func_ptr: carresser_le_capitaine as *mut std::os::raw::c_void,
+                    signature: signature.as_ptr() as *const c_char,
+                    attachment: std::ptr::null_mut()
+                }
+            }
+        }];
+
+        let module_name = CString::new("env").unwrap();
+
+        if !wasm_runtime_register_natives(module_name.as_ptr() as *const c_char, ntv.as_mut_ptr(), ntv.len() as u32) {
+            Error!("Failed to register FFI");
+        }
+
+        let module = wasm_runtime_load(
+            wasm_module_bytes_2.as_mut_ptr(),
+            wasm_module_bytes.len() as u32,
+            error_buf.as_mut_ptr(),
+            error_buf.len() as u32,
+        );
+
+        if module.is_null() {
+            panic!(
+                "Module is null: {}",
+                CStr::from_ptr(error_buf.as_ptr()).to_string_lossy()
+            );
+        }
+
+        let args = vec![CString::new("test").unwrap()];
+
+        let c_args = args
+            .iter()
+            .map(|arg| arg.as_ptr())
+            .collect::<Vec<*const c_char>>();
+
+        let null_ptr = 0 as *mut *const i8;
+
+        wasm_runtime_set_wasi_args(
+            module,
+            null_ptr,
+            0,
+            null_ptr,
+            0,
+            null_ptr,
+            0,
+            c_args.as_ptr() as *mut *mut i8,
+            c_args.len() as i32,
+        );
+
+        let module_inst = wasm_runtime_instantiate(
+            module,
+            STACK_SIZE,
+            8 * 1024,
+            error_buf.as_mut_ptr(),
+            error_buf.len() as u32,
+        );
+
+        if module_inst.is_null() {
+            panic!(
+                "Instatiated module is null: {}",
+                CStr::from_ptr(error_buf.as_ptr()).to_string_lossy()
+            );
+        }
+
+        let exec_env = wasm_runtime_create_exec_env(module_inst, STACK_SIZE);
+
+        if exec_env.is_null() {
+            panic!(
+                "Exec env is null: {}",
+                CStr::from_ptr(error_buf.as_ptr()).to_string_lossy()
+            );
+        }
+
+        let start_func = wasm_runtime_lookup_wasi_start_function(module_inst);
+
+        if start_func.is_null() {
+            panic!("start func is null");
+        }
+
+        let mut results: Vec<wasm_val_t> = vec![];
+        let mut args: Vec<wasm_val_t> = vec![];
+
+        if wasm_runtime_call_wasm_a(
+            exec_env,
+            start_func,
+            results.len() as u32,
+            results.as_mut_ptr(),
+            args.len() as u32,
+            args.as_mut_ptr(),
+        ) {
+            println!("success");
+        }
+    }
 }
 
 /*
