bcf7c37699df7c3eaeda94ae8a798265296ed587
Alix ANNERAUD <alix@anneraud.fr>
2024-07-12 00:51:51 -0400

    Start to write initialize

--- Modules/Initialize/src/lib.rs
+++ Modules/Initialize/src/lib.rs
@@ -1,14 +1,34 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use log::{error, info};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn Initialize() {
+    info!("Initializing...");
+
+    info!("Initialize task module");
+
+    if let Err(Error) = Task::Initialize() {
+        error!("Failed to initialize the task module: {:?}", Error);
+        std::process::exit(1);
     }
+
+    info!("Initialization user module");
+
+    if let Err(Error) = Users::Initialize() {
+        error!("Failed to initialize the user module: {:?}", Error);
+        std::process::exit(1);
+    }
+
+    info!("Initialize file system module");
+
+    if let Err(Error) = File_system::Initialize() {
+        error!("Failed to initialize the file system module: {:?}", Error);
+        std::process::exit(1);
+    }
+
+    info!("Initialize virtual machine module");
+
+    info!("Initialization complete");
 }
